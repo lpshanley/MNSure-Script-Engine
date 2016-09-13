0d6bd457558c061094a4551bafa2c877ff415539
lpshanley <lpshanley@gmail.com>
2016-09-13 13:57:56 -0500

    Updated domTools modal writing
    Updated the domTools modal writing tools to report logs for diagnostics.

--- js/app.js
+++ js/app.js
@@ -186,34 +186,57 @@ var _engine = {
 				contactTab: {
 					caseNoteModal:{
 						subject: function( _s ){
+							
+							_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.subject() ] Started | Input: " + _s);
+							
 							if(typeof _engine.domTools.get.icFrame.contactTab.caseNoteModal._subject() != 'undefined'){
+								
+								_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.subject() ] Located modal subject");
+								
 								_engine.domTools.get.icFrame.contactTab.caseNoteModal._subject().val( _s );
+								
 							} else {
 								_engine.debug.warn("- * Fail Reason: [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.subject() ]: Could not add line. Subject returning 'undefined'.")
 							}
+							
+							_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.subject() ] Complete");
+							
 						},
 						body: {
 							addLine: function( _s ){
+								
+								_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine() ] Started | Input: " + _s);
+								
 								if(typeof _engine.domTools.get.icFrame.contactTab.caseNoteModal._body() != 'undefined'){
+									
+									_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine() ] Located modal body");
+									
 									//Grab Modal Body
 									var modalBody = _engine.domTools.get.icFrame.contactTab.caseNoteModal._body();
 									//Wrap input in div tags
 									var line = $('<div>',{'html':_s});									
 									//Check if body is empty
 									if(_engine.domTools.test.icFrame.contactTab.caseNoteModal.body.isEmpty()){
+										
+										_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine() ] Body empty. Writing Line.");
+										
 										//If empty, set first line
 										$( modalBody ).html( line );
 									} else {
+										
+										_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine() ] Body not empty. Writing Line.");
+										
 										//If not empty, add to body
 										$( modalBody ).append( line );
 									}
+									
 								} else {
 									_engine.debug.warn("- * Fail Reason: [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine() ]: Could not add line. Body returning 'undefined'.")
 								}
-							},
-							addDivider: function( _s ){
-								_engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine( "-----------------------------" );
-							},
+								
+								_engine.debug.info("- * [ _engine.domTools.set.icFrame.contactTab.caseNoteModal.body.addLine() ] Complete");
+								
+							}
 						}
 					}
 				}
