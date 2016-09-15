7feacc5dbc6cc212863bbb789915180390151936
lpshanley <lpshanley@gmail.com>
2016-09-15 13:35:32 -0500

    added in error html for null response

--- js/app.js
+++ js/app.js
@@ -1131,11 +1131,15 @@ var _engine = {
 			chrome.runtime.sendMessage( _engine.advanced.extensionID(), { file: _f, commit: _c },
 				function( response ){
 					
-					console.log( typeof response );
-					console.log( response == null );
-					console.log( response );
-					
-					_engine.storage.html.set( response );
+					if( response == null ){
+						
+						_engine.advanced.getView( "error/error.html" );
+						
+					} else {
+						
+						_engine.storage.html.set( response );
+						
+					}
 					
 				}
 			);
