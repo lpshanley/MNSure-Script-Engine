25f4d654fc0684dc96223c752cc5ab3dfcf5a32b
lpshanley <lpshanley@gmail.com>
2016-09-23 11:58:29 -0500

    Updated tab closure logic

--- js/app.js
+++ js/app.js
@@ -1812,8 +1812,12 @@ var _engine = {
 		/* [Tools] Closes currently open tab
 		/********************************************************************/
 		
-		closeCurrentTab: function(){
-			$( _engine.domTools.get.hcrTabActive() ).find('span.dijitTabCloseButton').click();
+		closeTabHCR: function( _tab ){
+			
+			typeof _tab == 'undefined' ? _tab = _engine.domTools.get.hcrTabActive() : _tab = _tab[0];
+			
+			$( _tab ).find('span.dijitTabCloseButton').click();
+			
 		},
 		
 		/* [Tools] Chooses a specified result on the page
