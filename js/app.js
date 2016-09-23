0503f347daa4d101a85d018d53ef689685e20822
lpshanley <lpshanley@gmail.com>
2016-09-23 10:23:30 -0500

    added advanced query to searches

--- js/app.js
+++ js/app.js
@@ -252,6 +252,38 @@ var _engine = {
 						
 					}
 					
+				},
+				
+				advancedQuery: function( _query ){
+					
+					var screenType = _engine.domTools.test.hcrTabActiveType();
+					
+					if( screenType == "Case Search" || screenType == "Person Search" ){
+						
+						var _frame = $( _engine.domTools.get.hcrTabActiveFrame() );
+						
+						var _result = $( _frame ).find('iframe').contents().find( _query );
+						
+						if( _result.length > 0 ){
+							
+							return _result;
+							
+						} else {
+							
+							_engine.debug.error("- * [ _engine.domTools.get.searches.advancedQuery( _query ) ] Could not find requested field: " + _field);
+							
+							return false;
+							
+						}
+						
+					} else {
+						
+						_engine.debug.error("- * [ _engine.domTools.get.searches.advancedQuery( _query ) ] You must be on a search page to use this dom query.");
+
+						return false;
+						
+					}
+					
 				}
 				
 			}
