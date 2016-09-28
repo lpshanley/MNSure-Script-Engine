e549c55c8ed1657318236908fa633e6f47f96a04
lpshanley <lpshanley@gmail.com>
2016-09-28 14:11:36 -0500

    Updated navi to new main tab navigation

--- js/app.js
+++ js/app.js
@@ -700,8 +700,6 @@ var _engine = {
 					
 				}
 				
-				if(typeof callback === "function") callback();
-				
 			}
 		
 		},
@@ -1965,52 +1963,54 @@ var _engine = {
 
 			start: function(){
 				
-				_engine.navigation.hcr();
+				_engine.navigation.mainTabs.mainTabNavi('hcr',function(){
 				
-				var _modalType = "queries";
-				var _title = "Unified Search Query";
-				
-				var _url = _engine.advanced.baseUrl();
-				_url += "views/queries/unified search.html"
-				
-				_engine.advanced.getView( _url );
-				
-				// Check every 100ms for info in local storage. Timeout after 2000ms.
+					var _modalType = "queries";
+					var _title = "Unified Search Query";
 					
-				var _c = 0;
-				
-				var buildFrame = setInterval(function(){
-					if(_c <= _engine.advanced._vars.iterations){
+					var _url = _engine.advanced.baseUrl();
+					_url += "views/queries/unified search.html"
+					
+					_engine.advanced.getView( _url );
+					
+					// Check every 100ms for info in local storage. Timeout after 2000ms.
 						
-						if( _engine.storage.html.get() != false ){
-							// Gather html for modal
-							var _html = _engine.storage.html.get();
+					var _c = 0;
+					
+					var buildFrame = setInterval(function(){
+						if(_c <= _engine.advanced._vars.iterations){
 							
-							if( $('<div>', {'html': _engine.storage.html.get() }).find('div').hasClass('mns-error-modal') ){
-								_title = "Error";
-								_modalType = "error";
+							if( _engine.storage.html.get() != false ){
+								// Gather html for modal
+								var _html = _engine.storage.html.get();
+								
+								if( $('<div>', {'html': _engine.storage.html.get() }).find('div').hasClass('mns-error-modal') ){
+									_title = "Error";
+									_modalType = "error";
+								}
+								
+								// Clear html storage
+								_engine.storage.html.clear();
+								
+								//Build modal
+								_engine.ui.modal.build( _title, _html, _modalType );
+								
+								clearInterval( buildFrame );
+								
 							}
 							
-							// Clear html storage
+							_c++;
+							
+						} else {
+							_engine.debug.error("- * Fail Reason: [ _engine.caseWork.unifiedSearch.start() ]: Build frame html timed out.");	
 							_engine.storage.html.clear();
-							
-							//Build modal
-							_engine.ui.modal.build( _title, _html, _modalType );
-							
 							clearInterval( buildFrame );
-							
 						}
-						
-						_c++;
-						
-					} else {
-						_engine.debug.error("- * Fail Reason: [ _engine.caseWork.unifiedSearch.start() ]: Build frame html timed out.");	
-						_engine.storage.html.clear();
-						clearInterval( buildFrame );
-					}
-				}, _engine.advanced._vars.timeout);
+					}, _engine.advanced._vars.timeout);
+					
+					buildFrame;
 				
-				buildFrame;
+				});
 				
 			},
 
