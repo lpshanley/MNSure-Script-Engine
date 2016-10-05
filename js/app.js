9984f268f5c354ca7debb2917ff66f27ceeb4c9a
lpshanley <lpshanley@gmail.com>
2016-10-05 08:44:25 -0500

    fixed syntax error in error reporting

--- js/app.js
+++ js/app.js
@@ -438,13 +438,13 @@ var _engine = {
 						
 					} else {
 						
-						_engine.debug.warn('You are not on an integrated case. Current tab type is: ${ hcrTabType }.');
+						_engine.debug.warn(`You are not on an integrated case. Current tab type is: ${ hcrTabType }.`);
 						
 					}
 					
 				} else {
 					
-					_engine.debug.warn('Current HCR Tab can only be determined if on the HCR Cases and Outcomes screen. Current main tab is: ${ mainTabType }');
+					_engine.debug.warn(`Current HCR Tab can only be determined if on the HCR Cases and Outcomes screen. Current main tab is: ${ mainTabType }`);
 					
 				}
 				
