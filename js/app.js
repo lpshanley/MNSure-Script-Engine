65190eb46b800a84f064791c928ed7975dfcc4a6
lpshanley <lpshanley@gmail.com>
2016-10-18 11:20:19 -0400

    Fixed syntax error

--- js/app.js
+++ js/app.js
@@ -3055,7 +3055,7 @@ var _engine = {
 		getCommit: function( commit ){
 			if(typeof commit === 'undefined') commit = _engine.storage.config.get('commit.current');
 			return _engine.storage.config.get('commit.'+commit);
-		}
+		},
 		
 		/* [Advanced] ajax request to grab html template for modals
 		/********************************************************************/
