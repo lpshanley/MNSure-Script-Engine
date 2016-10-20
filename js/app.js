446822c7b2f5ec39a1f872a83eb08305a2d6cbb2
lpshanley <lpshanley@gmail.com>
2016-10-20 12:35:24 -0400

    Finished pushing domTools to modules

--- js/app.js
+++ js/app.js
@@ -11,75 +11,6 @@
 
 var _engine = {
 	
-	//***************//
-	//* DOM Toolbox *//
-	//***************//
-	
-	domTools: {
-		
-		/* [DOM Toolbox] Performs logic operations and comparisons on DOM
-		/********************************************************************/
-		
-		test: {
-			searches: {
-				windowLoaded: function(){
-					
-					var _screenType = _engine.domTools.test.hcrTabType();
-					
-					if( _screenType == "Person Search" || _screenType == "Case Search" ){
-					
-						var _searchFrame = _engine.domTools.get.hcrTabFrame();
-						
-						if( typeof _searchFrame != "undefined" ){
-							
-							var _searchBody = $( _searchFrame ).find('iframe').contents().find('input');
-							
-							
-							//Search is open
-							if ( typeof _searchBody[0] != 'undefined' ){
-								
-								if( _engine.domTools.get.searches.advancedQuery(".action-set a:contains('Search')").length > 0 ){
-								
-									//Search is loaded
-									_engine.debug.info("- * [ _engine.domTools.test.searches.windowLoaded() ] Search is open and fully loaded.");
-									return true;
-								
-								} else {
-									
-									_engine.debug.warn("- * Fail Reason: [ _engine.domTools.test.searches.windowLoaded() ] Search is open but not fully loaded.");
-									return false;
-									
-								}
-								
-							} else {
-								
-								_engine.debug.warn("- * Fail Reason: [ _engine.domTools.test.searches.windowLoaded() ] Search is open but not fully loaded.");
-								return false;
-								
-							}
-						} else {
-							
-							_engine.debug.warn("- * Fail Reason: [ _engine.domTools.test.searches.windowLoaded() ] Unable to target search.");
-							return false;
-							
-						}
-					
-					} else {
-						
-						_engine.debug.warn("- * Fail Reason: [ _engine.domTools.test.searches.windowLoaded() ] Not on a valid search screen.");
-						return false;
-						
-					}
-					
-				}
-			}			
-		}
-	},
-	
-	//***************//
-	//*     Nav     *//
-	//***************//
-	
 	navigation: {
 		
 		mainTabs: {
@@ -2141,4 +2072,4 @@ _engine.temp.jQloaded = setInterval(function(){
 		clearInterval(_engine.temp.jQloaded);
 	}
 },25);
-_engine.temp.jQloaded;;
+_engine.temp.jQloaded;
