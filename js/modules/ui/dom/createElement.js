96838e66a5e6adefb714fcfa4e9f582350b8c39b
lpshanley <lpshanley@gmail.com>
2016-10-21 11:30:29 -0400

    Pushing addition of merlin core

---
+++ js/modules/ui/dom/createElement.js
@@ -0,0 +1,29 @@
+/* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
+_engine.module.define('ui/dom/createElement',function( input ){
+
+	let config = {
+		type: '',
+		styles: '',
+		classes: '',
+		id: '',
+		content: ''
+	};
+
+	$.each(input,function(k,v){
+		config[k] = input[k];
+
+	});
+
+	if( config.type === '' ) return false;
+	
+	let newEle = $('<'+input.type+'>',{
+		'data-merlin-id': config.id,
+		'data-merlin-attach': config.attachId,
+		'style': config.styles,
+		'class': config.classes,
+		html: config.content
+	});
+
+	return newEle;
+
+});
