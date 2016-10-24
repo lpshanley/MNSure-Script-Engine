1b5a76dcdc9940a0f17777d7414e67af3681a497
Lucas Shanley <lpshanley@users.noreply.github.com>
2016-10-24 10:10:07 -0500

    Fixed Evidence type for address

--- js/app.js
+++ js/app.js
@@ -3056,7 +3056,7 @@ var _engine = {
 			iterations: 80,
 			iterationsLong: 200,
 			queryDefinitions: {
-				'address': 'evidenceType=DET0026039',
+				'address': 'evidenceType=PDC0000261',
 				'income': 'evidenceType=DET0026030',
 				'service agency': 'evidenceType=DET0001029',
 				'financially responsible agency': 'evidenceType=DET0001282'
