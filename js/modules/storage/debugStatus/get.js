1c13cbe30dd4674385216bb96b6d232e0d70dea1
lpshanley <lpshanley@gmail.com>
2016-10-25 11:47:33 -0400

    Fixed unset type error

--- js/modules/storage/debugStatus/get.js
+++ js/modules/storage/debugStatus/get.js
@@ -2,7 +2,7 @@
 _engine.module.define('storage/debugStatus/get',function(){
 
 	if( typeof window.localStorage.mnsEngine_debugStatus === 'undefined' ){
-		_engine.storage.mnsEngine_debugStatus = false;
+		window.localStorage.mnsEngine_debugStatus = false;
 	}
 
 	return String( window.localStorage.mnsEngine_debugStatus.toLowerCase() ) == "true";
