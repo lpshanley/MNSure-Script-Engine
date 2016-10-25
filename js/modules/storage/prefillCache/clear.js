0ff730b16e8146ff029f1f0103d1f7bf31b1986e
lpshanley <lpshanley@gmail.com>
2016-10-25 12:42:41 -0400

    Fixing error in prefill cache clear

--- js/modules/storage/prefillCache/clear.js
+++ js/modules/storage/prefillCache/clear.js
@@ -1,6 +1,8 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('storage/prefillCache/clear',function(){
-
-	delete _engine.storage.nocache.data.caseData.prefill;
+	
+	if( typeof _engine.storage.nocache.data.caseData.prefill !== 'undefined' ){
+		delete _engine.storage.nocache.data.caseData.prefill;
+	}
 
 });
