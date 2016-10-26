52d33af12ac94211ec4f957a3b584f39115443ef
lpshanley <lpshanley@gmail.com>
2016-10-26 16:40:54 -0400

    Testing timeout/refresh logicl

--- js/modules/advanced/setupTimeoutAlert.js
+++ js/modules/advanced/setupTimeoutAlert.js
@@ -11,6 +11,6 @@ _engine.module.define('advanced/setupTimeoutAlert',function(){
 			window.location.reload();
 			
 		}
-	}, _engine.advanced._sessionRemaining() - tripTimer);
+	}, 1770000);//_engine.advanced._sessionRemaining() - tripTimer);
 	
 });
