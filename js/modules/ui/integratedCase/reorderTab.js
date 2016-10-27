5f5f8c4ba2508e9cc1e8abeb42b1346d9390ff2d
lpshanley <lpshanley@gmail.com>
2016-10-27 14:10:24 -0400

    Pushing function to reorder tab locations

---
+++ js/modules/ui/integratedCase/reorderTab.js
@@ -0,0 +1,25 @@
+_engine.module.define('ui/integratedCase/reorderTab',function( target, insertAfter ){
+	
+	if( typeof target === 'string' && typeof insertAfter === 'string' && target !== insertAfter ){
+		
+		target = target.toLowerCase();
+		insertAfter = insertAfter.toLowerCase();
+		
+		$.each(_engine.domTools.get.icFrame.icTabList(),function(key,tab){
+			
+			let tabTitle = $(tab)[0].innerText.toLowerCase().trim();
+			
+			if( tabTitle === target ) target = tab;
+			if( tabTitle === insertAfter ) insertAfter = tab;
+			
+		});
+		
+		if( typeof target === 'object' && typeof insertAfter === 'object' ){
+			
+			$( target ).insertAfter( $( insertAfter ) );
+			
+		}
+		
+	}
+	
+});
