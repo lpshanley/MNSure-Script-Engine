0a57ea2328e78c5b996608eb668dedbfc4b99c61
lpshanley <lpshanley@gmail.com>
2016-10-28 15:13:28 -0400

    Pushing updated click event handler

--- js/modules/events/handleClickEvent.js
+++ js/modules/events/handleClickEvent.js
@@ -1,8 +1,37 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('events/handleClickEvent',function( req ){
 	
-	console.log( _engine.tools.stringToQuery( req ) );
-
+	req = _engine.tools.stringToQuery( req );
+	
+	let last = req.length - 1;
+	
+	let eventManager = _engine;
+	
+	$.each(req,function(key,value){
+		
+		let param;
+		
+		if( value.indexOf("(") !== -1 ){
+			
+			param = value.substring(value.indexOf("(")+1,value.indexOf(")")); 
+			value = value.substring(0,value.indexOf("(")); 
+			
+			console.log(`Value: ${value}`);
+			console.log(`Param: ${param}`);
+			
+		}
+		
+		eventManager = eventManager[value];
+		
+		console.log( eventManager );
+		
+		if( key === last ){
+			
+			console.log( 'Last' );
+			
+		}
+	
+	});
 	
 	/*
 	var eventLog = e.split('/');
