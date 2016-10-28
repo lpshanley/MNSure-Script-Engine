27ea016bef80d3b45c3ed0d030675e84857424c2
lpshanley <lpshanley@gmail.com>
2016-10-28 15:33:41 -0400

    Pushed event manager to perform with no logging

--- js/modules/events/handleClickEvent.js
+++ js/modules/events/handleClickEvent.js
@@ -16,14 +16,9 @@ _engine.module.define('events/handleClickEvent',function( req ){
 			param = value.substring(value.indexOf("(")+1,value.indexOf(")")); 
 			value = value.substring(0,value.indexOf("(")); 
 			
-			console.log(`Value: ${value}`);
-			console.log(`Param: ${param}`);
-			
 		}
 		
 		eventManager = eventManager[value];
-		
-		console.log( typeof eventManager );
 		
 		if( key === last ){
 			
