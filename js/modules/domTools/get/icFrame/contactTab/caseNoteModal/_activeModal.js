9ebe395ca904a88e1712374e4b311dae80bb3bca
lpshanley <lpshanley@gmail.com>
2016-11-01 17:31:55 -0400

    Pushing new case mote modal script

--- js/modules/domTools/get/icFrame/contactTab/caseNoteModal/_activeModal.js
+++ js/modules/domTools/get/icFrame/contactTab/caseNoteModal/_activeModal.js
@@ -1,5 +1,7 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('domTools/get/icFrame/contactTab/caseNoteModal/_activeModal',function(){
+_engine.module.define('domTools/get/icFrame/contactTab/caseNoteModal/_activeModal',function( logMsg ){
+	
+	if( typeof logMsg !== 'boolean' ) logMsg = true;
 	
 	var _modalFrame = $('iframe[title="Modal Frame - New Note"].curam-active-modal');
 
@@ -9,17 +11,32 @@ _engine.module.define('domTools/get/icFrame/contactTab/caseNoteModal/_activeModa
 
 		//Modal is open
 		if ( typeof _bodyFrame[0] != 'undefined' ){
-
-			//Modal is loaded
-			_engine.debug.info("- * [ _engine.domTools.get.icFrame.contactTab.caseNoteModal._activeModal() ]: Modal is open and fully loaded.");
-			return _modalFrame;
-
+			
+			var _modalBody = $( _bodyFrame ).contents().find('body');
+			
+			if( _modalBody.length > 0 ){
+				
+				//Modal is loaded
+					_engine.debug.info("- * [ _engine.domTools.get.icFrame.contactTab.caseNoteModal._activeModal() ]: Modal is open and fully loaded.");
+				return _modalFrame;
+				
+			} else {
+				if(logMsg){
+				_engine.debug.warn("- * Fail Reason: [ _engine.domTools.get.icFrame.contactTab.caseNoteModal._activeModal() ]: Modal is open but not fully loaded.");
+				}
+				return false;
+			}
+			
 		} else {
+			if(logMsg){
 			_engine.debug.warn("- * Fail Reason: [ _engine.domTools.get.icFrame.contactTab.caseNoteModal._activeModal() ]: Modal is open but not fully loaded.");
+			}
 			return false;
 		}
 	} else {
+		if(logMsg){
 		_engine.debug.warn("- * Fail Reason: [ _engine.domTools.get.icFrame.contactTab.caseNoteModal._activeModal() ]: Unable to target an open case note modal.");
+		}
 		return false;
 	}
 	
