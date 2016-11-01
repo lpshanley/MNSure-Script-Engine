5238177dba61b8497309ff84680c2090b8319ca1
lpshanley <lpshanley@gmail.com>
2016-11-01 14:15:54 -0400

    Setup new modal launcher and handler

--- js/modules/ui/modal/_button.js
+++ js/modules/ui/modal/_button.js
@@ -1,6 +1,8 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('ui/modal/_button',function( _type ){
-
+	
+	console.log( _type );
+	
 	_engine.debug.info( "- * [ _engine.ui.modal._button() ] function started with type: " + _type );
 
 	switch( _type.toLowerCase() ){
@@ -55,5 +57,5 @@ _engine.module.define('ui/modal/_button',function( _type ){
 			break;
 
 	}
-
+	
 });
