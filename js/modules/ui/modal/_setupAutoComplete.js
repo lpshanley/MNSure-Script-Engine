3102947acdc3ffa5433df49223ef9d57982dddab
lpshanley <lpshanley@gmail.com>
2016-11-03 09:46:08 -0400

    Pushing completed auto complete script

--- js/modules/ui/modal/_setupAutoComplete.js
+++ js/modules/ui/modal/_setupAutoComplete.js
@@ -1,11 +1,11 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('ui/modal/_setupAutoComplete',function(){
 	
-	var _fields = $('.mns-modal-template > .mns-input-group');
+	let _fields = $('.mns-modal-template > .mns-input-group');
 		//Push additional clusters if clustering is active
 	if( _engine.ui.modal._clustersActive() ){
 		
-		var _clusterFields = $('.mns-modal-template > .mns-input-cluster.input-cluster-active > .mns-input-group');
+		let _clusterFields = $('.mns-modal-template > .mns-input-cluster.input-cluster-active > .mns-input-group');
 		
 		$.each(_clusterFields,function(k,v){ _fields.push(v); });
 	
@@ -15,23 +15,32 @@ _engine.module.define('ui/modal/_setupAutoComplete',function(){
 		
 		if( $( v ).find('input').length !== 0 ){
 			
-			var input = $( v ).find('input')[0];
+			let input = $( v ).find('input')[0];
 			
 			if( typeof $( input ).attr('data-autoComplete') !== 'undefined' ){
 				
 				//Process prefill on field(s)
 				
-				var _autoComplete = $( input ).attr('data-prefill').toLowerCase();
+				let _autoComplete = $( input ).attr('data-autoComplete').toLowerCase();
 				
-				var _autoCompleteType = _prefill.split("|")[0];
-				var _autoCompleteValue = _prefill.split("|")[1];
+				let _autoCompleteType = _autoComplete.split("|")[0];
+				let _autoCompleteValue = _autoComplete.split("|")[1];
+				
+				let autoCompleteSource = [];
 				
 				switch( _autoCompleteType ){
 					case "participants":
 						switch( _autoCompleteValue ){
 							case "name":
 								
-								console.log('FOUND');
+								$.each(_engine.storage.nocache.query('caseData.participants'),function(k,v){
+									autoCompleteSource.push(v.name) 
+								});
+								
+								$( input ).autocomplete({
+									minLength: 0,
+									source: autoCompleteSource
+								});
 								
 								break;
 							default:
