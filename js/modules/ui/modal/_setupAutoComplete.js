45d04ebc09e39e5827a97ad1559eca3a338446b1
lpshanley <lpshanley@gmail.com>
2016-11-03 16:58:22 -0400

    Pushing autoFocus to autocomplete data

--- js/modules/ui/modal/_setupAutoComplete.js
+++ js/modules/ui/modal/_setupAutoComplete.js
@@ -40,6 +40,7 @@ _engine.module.define('ui/modal/_setupAutoComplete',function(){
 								$( input ).autocomplete({
 									minLength: 0,
 									delay: 0,
+									autoFocus: true,
 									source: autoCompleteSource
 								});
 								
