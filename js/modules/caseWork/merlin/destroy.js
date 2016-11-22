0b7bfcc13e6b12f5d30200e93a3efbf0b3e219a4
lpshanley <lpshanley@gmail.com>
2016-11-22 12:43:37 -0500

    Pushing update to modal storage binder and merlin ui

--- js/modules/caseWork/merlin/destroy.js
+++ js/modules/caseWork/merlin/destroy.js
@@ -3,6 +3,8 @@ _engine.module.define('caseWork/merlin/destroy',function( id ){
 	
 	$('[data-id="'+id+'"]').remove();
 	
-	delete _engine.storage.nocache.data.merlin.id;
+	delete _engine.storage.nocache.data.modal[id];
+	
+	if( Object.getOwnPropertyNames( _engine.storage.nocache.data.modal ).length === 0 ) _engine.ui.dom.dimLights( false );
 
 });
