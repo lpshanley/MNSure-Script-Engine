3d062e397cc9d548f25a0e4ed9c26d4e0a0a3f5b
lpshanley <lpshanley@gmail.com>
2016-11-22 16:39:36 -0500

    Testing loading angular js

--- js/modules/tools/loadAddons/config.js
+++ js/modules/tools/loadAddons/config.js
@@ -1,7 +1,8 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('tools/loadAddons/config',{
 	js: {
-		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"
+		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js",
+		"angularjs": "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"
 	},
 	css: {
 		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/redmond/jquery-ui.css"
