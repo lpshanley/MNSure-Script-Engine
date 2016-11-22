bb89de429ae3162883d37ecb2b377cc2d13b4906
lpshanley <lpshanley@gmail.com>
2016-11-22 16:58:16 -0500

    Removing angular

--- js/modules/tools/loadAddons/config.js
+++ js/modules/tools/loadAddons/config.js
@@ -1,8 +1,7 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('tools/loadAddons/config',{
 	js: {
-		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js",
-		"angularjs": "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"
+		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"
 	},
 	css: {
 		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/redmond/jquery-ui.css"
