65e929275dccbedf79ffdee8c11d9c7fdb01551d
lpshanley <lpshanley@gmail.com>
2016-12-22 14:39:51 -0500

    Adding new, more reliable, tab event handler

--- js/modules/events/domMonitor.js
+++ js/modules/events/domMonitor.js
@@ -1,6 +1,25 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('events/domMonitor',function(){
+	
+	if( typeof _engine.advanced._vars.lastTab === 'undefined' ) _engine.advanced._vars.lastTab = null;
+	
+	$( document ).on('click', '.dijitTabListWrapper .visible.dijitTab',function(){ 
+	
+		if( _engine.advanced._vars.lastTab !== this ){
 
+			console.log( "Navigated" );
+			console.log( "New:", this );
+
+			_engine.advanced._vars.lastTab = this;
+			
+			_engine.events.tabEventHandler( this );
+			
+		}
+
+	});
+	
+	/*
+	
 	if(typeof dojo.aspect === 'undefined') dojo.require("dojo.aspect");
 	
 	let mainTabs = dijit.registry.byId("app-sections-container-dc");
@@ -16,5 +35,7 @@ _engine.module.define('events/domMonitor',function(){
 		_engine.events.tabEventHandler( this.tablist._selectedTab );
 		console.log( this );
 	});
+	
+	*/
 	
 });
