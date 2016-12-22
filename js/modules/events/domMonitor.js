57b0fc20a7e20f291a37758d092e4f309a226187
lpshanley <lpshanley@gmail.com>
2016-12-22 14:58:18 -0500

    Added sytax to scan for new tab creation

--- js/modules/events/domMonitor.js
+++ js/modules/events/domMonitor.js
@@ -3,13 +3,11 @@ _engine.module.define('events/domMonitor',function(){
 	
 	if( typeof _engine.advanced._vars.lastTab === 'undefined' ) _engine.advanced._vars.lastTab = null;
 	
+	// Trips event monitor on navigation
 	$( document ).on('click', '.dijitTabListWrapper .visible.dijitTab',function(){ 
 	
 		if( _engine.advanced._vars.lastTab !== this ){
-
-			console.log( "Navigated" );
-			console.log( "New:", this );
-
+			
 			_engine.advanced._vars.lastTab = this;
 			
 			_engine.events.tabEventHandler( this );
@@ -18,24 +16,17 @@ _engine.module.define('events/domMonitor',function(){
 
 	});
 	
-	/*
-	
-	if(typeof dojo.aspect === 'undefined') dojo.require("dojo.aspect");
-	
-	let mainTabs = dijit.registry.byId("app-sections-container-dc");
-
-	dojo.aspect.after(mainTabs, "selectChild", function() {
-		_engine.events.tabEventHandler( this.tablist._selectedTab );
-		console.log( this );
+	// Trips event monitor when new tab is created as system auto navigates to the new tab
+	$( '.dijitTabListWrapper' ).on('DOMSubtreeModified', '.visible.dijitTab',function(){
+		
+		if( _engine.advanced._vars.lastTab !== this ){
+			
+			_engine.advanced._vars.lastTab = this;
+			
+			_engine.events.tabEventHandler( this );
+			
+		}
+		
 	});
-	
-	let HRCTabs = dijit.registry.byId("HCRCASEAPPWorkspaceSection-stc");
-
-	dojo.aspect.after(HRCTabs, "selectChild", function() {
-		_engine.events.tabEventHandler( this.tablist._selectedTab );
-		console.log( this );
-	});
-	
-	*/
 	
 });
--- js/modules/events/tabEventHandler.js
+++ js/modules/events/tabEventHandler.js
@@ -1,44 +1,44 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('events/tabEventHandler',function( tab ){
+	
+	if( typeof $( tab ).attr('widgetid') !== "undefined" ){
+	
+		let tabScope = $( tab ).attr('widgetid').split('-')[0];
 
-	let tabScope = $( tab ).attr('widgetid').split('-')[0];
-	
-	let contentId = dijit.registry.byNode( tab )._curamPageId;
-	
-	console.log( $('#'+contentId+' iframe') );
-	
-	switch( tabScope ){
-		case "HCRCASEAPPWorkspaceSection":
-			
-			
-			
-			_engine.caseWork.caseData.maintainNocache();
-			_engine.ui.integratedCase.interfaceAlteration();
-			
-			console.log('HCR')
-			
-			break;
-		case 'app':
-			
-			switch($(tab)[0].innerText.trim() ){
-				case 'HCR Cases and Outcomes':
-					
-					_engine.caseWork.caseData.maintainNocache();
-					_engine.ui.integratedCase.interfaceAlteration();
-					
-					break;
-				default:
-					break;
-			}
-			
-			break;
-		case 'HCRIntegratedCase':
-			
-			_engine.caseWork.caseData.maintainNocache();
-			
-			break;
-		default:
-			break;
+		let contentId = dijit.registry.byNode( tab )._curamPageId;
+
+		console.log( $('#'+contentId+' iframe') );
+
+		switch( tabScope ){
+			case "HCRCASEAPPWorkspaceSection":
+
+				_engine.caseWork.caseData.maintainNocache();
+				_engine.ui.integratedCase.interfaceAlteration();
+
+				break;
+			case 'app':
+
+				switch($(tab)[0].innerText.trim() ){
+					case 'HCR Cases and Outcomes':
+
+						_engine.caseWork.caseData.maintainNocache();
+						_engine.ui.integratedCase.interfaceAlteration();
+
+						break;
+					default:
+						break;
+				}
+
+				break;
+			case 'HCRIntegratedCase':
+
+				_engine.caseWork.caseData.maintainNocache();
+
+				break;
+			default:
+				break;
+		}
+		
 	}
 	
 });
