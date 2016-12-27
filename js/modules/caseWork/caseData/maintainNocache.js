12ac956461cfd488bd1acf359666ce384c50bf1c
lpshanley <lpshanley@gmail.com>
2016-12-27 10:24:02 -0500

    Pushing updated nocache maintenence functionality

--- js/modules/caseWork/caseData/maintainNocache.js
+++ js/modules/caseWork/caseData/maintainNocache.js
@@ -1,118 +1,119 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('caseWork/caseData/maintainNocache',function( callback ){
+
+	let cacheEmpty = typeof _engine.storage.nocache.data.caseData === 'undefined';
 	
-	_engine.domTools.test.hcrTabActiveIsIC(function( result ){
-		
-		let cacheEmpty = typeof _engine.storage.nocache.data.caseData === 'undefined';
-		
-		let curamObj = _engine.storage._curamCreatedObject.get();
-		
-		var updateCaseData = function( data ){
-			let parsedData = $.parseHTML( data );
+	let tabParams = curam.tab.getSelectedTab();
+	
+	let curamObj = tabParams.params.tabDescriptor;
 
-			let caseData = {
-				participants:{}
-			}
+	var updateCaseData = function( data ){
+		let parsedData = $.parseHTML( data );
 
-			$.each( parsedData, function(k,v){
-				if( typeof $(v).attr('id') !== 'undefined' && $(v).attr('id') === 'content' ){
+		let caseData = {
+			participants:{}
+		}
 
-					let parsedPageData = $.parseHTML( $( v ).find( '.list tbody script' )[0].innerHTML );
+		$.each( parsedData, function(k,v){
+			if( typeof $(v).attr('id') !== 'undefined' && $(v).attr('id') === 'content' ){
 
-					let count = 0;
+				let parsedPageData = $.parseHTML( $( v ).find( '.list tbody script' )[0].innerHTML );
 
-					$.each( parsedPageData, function(k,v){
+				let count = 0;
 
-						if( !$(v).hasClass('list-details-row') ){
-							var rowData = $(v).find('td');
+				$.each( parsedPageData, function(k,v){
 
-							let urlParams = {};
+					if( !$(v).hasClass('list-details-row') ){
+						var rowData = $(v).find('td');
 
-							$.each($(rowData[1]).find('a')[0].search.replace('?','').split('&'),function(k,v){
+						let urlParams = {};
 
-								v = v.split('=');
+						$.each($(rowData[1]).find('a')[0].search.replace('?','').split('&'),function(k,v){
 
-								urlParams[v[0]] = v[1];
+							v = v.split('=');
 
-							});
+							urlParams[v[0]] = v[1];
 
-							caseData.participants[count] = {
-								name: rowData[1].innerText.trim(),
-								role: rowData[2].innerText.trim(),
-								startDate: rowData[3].innerText.trim(),
-								endDate: rowData[4].innerText.trim(),
-								endReason: rowData[5].innerText.trim(),
-								_status: rowData[6].innerText.trim(),
-								url:{
-									path: $(rowData[1]).find('a')[0].pathname,
-									params: urlParams
-								}
+						});
+
+						caseData.participants[count] = {
+							name: rowData[1].innerText.trim(),
+							role: rowData[2].innerText.trim(),
+							startDate: rowData[3].innerText.trim(),
+							endDate: rowData[4].innerText.trim(),
+							endReason: rowData[5].innerText.trim(),
+							_status: rowData[6].innerText.trim(),
+							url:{
+								path: $(rowData[1]).find('a')[0].pathname,
+								params: urlParams
 							}
-
-							count++;
-
 						}
 
-					});
+						count++;
 
-				}
+					}
 
-			});
+				});
 
-			_engine.storage.nocache.data.caseData = caseData;
+			}
 
-			_engine.storage.nocache.data.caseData.caseID = curamObj.tabContent.parameters.caseID;
+		});
 
-			_engine.debug.info('Nocache: Case Data Refreshed');
-			
-			if(typeof callback === 'function') callback( _engine.storage.nocache.query('caseData') );
-			
-		}
+		_engine.storage.nocache.data.caseData = caseData;
 		
-		var getData = function( caseId ){
-			
-			if(typeof caseId === 'undefined') caseId = curamObj.tabContent.parameters.caseID;
-			
+		_engine.storage.nocache.data.caseData.caseID = curamObj.tabContent.parameters.caseID;
+
+		_engine.debug.info('Nocache: Case Data Refreshed');
+
+		if(typeof callback === 'function') callback( _engine.storage.nocache.query('caseData') );
+
+	}
+
+	var getData = function( caseId ){
+
+		if(typeof caseId === 'undefined') caseId = curamObj.tabContent.parameters.caseID;
+
+		$.ajax({
+			url: 'en_US/DefaultIC_listCaseMemberPage.do',
+			data: {
+				o3ctx: curamObj.tabContent.parameters.o3ctx,
+				caseID: caseId,
+				o3nocache: curam.util.getCacheBusterParameter().split('=')[1]
+			},
+			async: true,
+			success: function(data){
+				updateCaseData( data );
+			}
+		});
+
+		if( Object.getOwnPropertyNames( _engine.advanced._vars.queryDefinitions ).length === 0 ){
 			$.ajax({
-				url: 'en_US/DefaultIC_listCaseMemberPage.do',
+				url:'en_US/HCRDefaultIC_dashboardPage.do',
 				data: {
 					o3ctx: curamObj.tabContent.parameters.o3ctx,
-					caseID: caseId,
-					o3nocache: curam.util.getCacheBusterParameter().split('=')[1]
-				},
-				async: true,
-				success: function(data){
-					updateCaseData( data );
+					caseID: curamObj.tabContent.parameters.caseID
 				}
-			});
-			
-			if( Object.getOwnPropertyNames( _engine.advanced._vars.queryDefinitions ).length === 0 ){
-				$.ajax({
-					url:'en_US/HCRDefaultIC_dashboardPage.do',
-					data: {
-						o3ctx: curamObj.tabContent.parameters.o3ctx,
-						caseID: curamObj.tabContent.parameters.caseID
+			}).success(function( data ){
+				$.each($.parseHTML( data ),function(k,v){
+					if(typeof $(v).attr('id') !== 'undefined' && $(v).attr('id') === 'content'){
+						$.each($(v).find('div#dashboardData li'),function(k,v){
+							_engine.advanced._vars.queryDefinitions[$(v).attr('name').toLowerCase()] = 'evidenceType=' + $(v).attr('evidencetype');
+						});
 					}
-				}).success(function( data ){
-					$.each($.parseHTML( data ),function(k,v){
-						if(typeof $(v).attr('id') !== 'undefined' && $(v).attr('id') === 'content'){
-							$.each($(v).find('div#dashboardData li'),function(k,v){
-								_engine.advanced._vars.queryDefinitions[$(v).attr('name').toLowerCase()] = 'evidenceType=' + $(v).attr('evidencetype');
-							});
-						}
-					});
-					_engine.debug.info('Nocache: Updated query definitions.');
 				});
-				
-			}
-			
+				_engine.debug.info('Nocache: Updated query definitions.');
+			});
 
 		}
-		
-		if(result){
+
+
+	}
+	
+	switch( tabParams.tabDescriptor.tabID ){
+		case 'HCRIntegratedCase':
 			
 			if(cacheEmpty) getData();
-			
+				
 			else {
 				
 				if( _engine.storage.nocache.data.caseData.caseID !== curamObj.tabContent.parameters.caseID){
@@ -121,49 +122,47 @@ _engine.module.define('caseWork/caseData/maintainNocache',function( callback ){
 				}
 				
 			}
+			
+			break;
+			
+		case 'PersonHome':
 				
-		} else {
+			if(!cacheEmpty){
 				
-			switch(curamObj.tabID){
-				case 'PersonHome':
-
-					if(!cacheEmpty){
-
-						var participants = _engine.storage.nocache.query('caseData.participants');
-
-						var roleIDs = [];
-
-						$.each( participants,function(k, participant){
-
-							roleIDs.push( participant.url.params.concernRoleID );
-
-						});
-
-						if( roleIDs.indexOf( curamObj.tabContent.parameters.concernRoleID ) === -1 ){
-							_engine.debug.info('Nocache: Tab is not in scope of current nocache. Clearing nocache.');
-							_engine.storage.nocache.delete('caseData');
-						}
-
-					}
-
-					break;
-				case 'EvidenceType':
-					if(!cacheEmpty){
-						if( curamObj.tabContent.parameters.caseID !== _engine.storage.nocache.query('caseData.caseID') ){
-							_engine.debug.info('Nocache: Tab is not in scope of current nocache. Clearing nocache.');
-							_engine.storage.nocache.delete('caseData');
-							getData( curamObj.tabContent.parameters.caseID );
-						}
-					}
-					else {
-						getData( curamObj.tabContent.parameters.caseID );
-					}
-					break;
-				default:
-					break;
+				var participants = _engine.storage.nocache.query('caseData.participants');
+				
+				var roleIDs = [];
+				
+				$.each( participants,function(k, participant){
+					
+					roleIDs.push( participant.url.params.concernRoleID );
+					
+				});
+					
+				if( roleIDs.indexOf( curamObj.tabContent.parameters.concernRoleID ) === -1 ){
+					_engine.debug.info('Nocache: Tab is not in scope of current nocache. Clearing nocache.');
+					_engine.storage.nocache.delete('caseData');
+				}
+				
 			}
 			
-		}
+			break;
+			
+		case 'EvidenceType':
+			if(!cacheEmpty){
+				if( curamObj.tabContent.parameters.caseID !== _engine.storage.nocache.query('caseData.caseID') ){
+					_engine.debug.info('Nocache: Tab is not in scope of current nocache. Clearing nocache.');
+					_engine.storage.nocache.delete('caseData');
+					getData( curamObj.tabContent.parameters.caseID );
+				}
+			}
+			else {
+				getData( curamObj.tabContent.parameters.caseID );
+			}
+			break;
+			
+		default:
+			break;
+	}
 
-	});
 });
