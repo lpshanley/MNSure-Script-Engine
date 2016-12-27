ce81b15e46f69f75bf614ce962935f433c458858
lpshanley <lpshanley@gmail.com>
2016-12-27 11:07:07 -0500

    Pushing set logic to reorder tabs

--- js/modules/domTools/get/icFrame/icTabList.js
+++ js/modules/domTools/get/icFrame/icTabList.js
@@ -1,11 +1,16 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('domTools/get/icFrame/icTabList',function(){
 	
-	if(_engine.domTools.test.hcrTabActiveIsIC()){
-		var _tp = _engine.domTools.get.hcrTabFrame();
-		return $( _tp ).find('div.dijitTabNoLayout[role="tablist"] > div.dijitTab.visible');
-	} else {
-		_engine.caseWork.caseSelection();
+	if(typeof curam.tab.getSelectedTab().params.tabDescriptor.tabID !== 'undefined'){
+		if(curam.tab.getSelectedTab().params.tabDescriptor.tabID === 'HCRIntegratedCase'){
+			var _tp = _engine.domTools.get.hcrTabFrame();
+			return $( _tp ).find('div.dijitTabNoLayout[role="tablist"] > div.dijitTab.visible');
+		} else {
+			_engine.caseWork.caseSelection();
+		}
+	}
+	else{
+		_engine.debug.warn("[icTabList] Attemping to gather tab list, curam tab params unavailable.");
 	}
 	
 });
