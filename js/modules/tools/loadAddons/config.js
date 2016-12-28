d93e630dfc37810d6dc24bd26c9d62af3ac13750
lpshanley <lpshanley@gmail.com>
2016-12-28 13:00:44 -0500

    Revert progress test

--- js/modules/tools/loadAddons/config.js
+++ js/modules/tools/loadAddons/config.js
@@ -1,8 +1,7 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('tools/loadAddons/config',{
 	js: {
-		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js",
-		"jqProgress": "https://cdn.rawgit.com/likerRr/jq-ajax-progress/b26b9f3/src/jq-ajax-progress.min.js"
+		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"
 	},
 	css: {
 		"jqueryui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/redmond/jquery-ui.css"
