543930cf937d6d7b47667bf330a1449a03a17a73
lpshanley <lpshanley@gmail.com>
2016-12-29 14:54:58 -0500

    Revamp of hcrTabActiveISIC

--- js/modules/domTools/get/icFrame/icTabActive.js
+++ js/modules/domTools/get/icFrame/icTabActive.js
@@ -1,6 +1,6 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('domTools/get/icFrame/icTabActive',function(){
-
+	
 	if( _engine.domTools.test.hcrTabActiveIsIC() ){
 		var _tp = _engine.domTools.get.hcrTabFrame();
 		return $( _tp ).find('div.dijitTabNoLayout[role="tablist"] > div.dijitTab.visible.dijitTabChecked.dijitChecked');
