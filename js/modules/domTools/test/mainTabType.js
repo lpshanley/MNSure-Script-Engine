374a8edd9bf02a8533ffa52e88003182446cf036
lpshanley <lpshanley@gmail.com>
2016-12-29 12:58:58 -0500

    Revamped mainTabType

--- js/modules/domTools/test/mainTabType.js
+++ js/modules/domTools/test/mainTabType.js
@@ -1,11 +1,9 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('domTools/test/mainTabType',function( callback ){
+
+	let label = dijit.registry.byNode( _engine.domTools.get.mainTabActive() ).label;
 	
-	var activeTab = _engine.domTools.get.mainTabActive();
-
-	var activeTabLabel = $( activeTab )[0].innerText.trim();
-
-	if( typeof callback === 'function' ) callback( activeTabLabel );
-	else return activeTabLabel;
+	if( typeof callback === 'function' ) callback( label );
+	else return label;
 	
 });
