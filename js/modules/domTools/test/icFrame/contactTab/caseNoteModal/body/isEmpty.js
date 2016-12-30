295a02b1bfeb8dfdb67c6aa5d394c3c164d56f6b
lpshanley <lpshanley@gmail.com>
2016-12-30 15:10:01 -0500

    Pushing local to alpha to debug mosal body empty

--- js/modules/domTools/test/icFrame/contactTab/caseNoteModal/body/isEmpty.js
+++ js/modules/domTools/test/icFrame/contactTab/caseNoteModal/body/isEmpty.js
@@ -2,13 +2,9 @@
 _engine.module.define('domTools/test/icFrame/contactTab/caseNoteModal/body/isEmpty',function(){
 	
 	let _modalBody = _engine.domTools.get.icFrame.contactTab.caseNoteModal._body(),
-			returnVal = false,
-			err = false;
+			returnVal = false;
 
 	if( _modalBody && $( _modalBody ).text() === "" ) returnVal = true;
-	else err = "Case note body object is returning undefined. Not loaded.";
-	
-	if( err ) _engine.debug.warn( `[ caseNoteModal.body.isEmpty() ]  ${ err }` );
 	
 	return returnVal;
 	
