1134c0cb3fbc5829cd07bcb195a64eb9bcfde47f
lpshanley <lpshanley@gmail.com>
2016-12-30 12:52:17 -0500

    Enhancing dom queries

--- js/modules/domTools/get/hcrTabList.js
+++ js/modules/domTools/get/hcrTabList.js
@@ -1,6 +1,15 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('domTools/get/hcrTabList',function(){
 	
-	return $('#HCRCASEAPPWorkspaceSection-stc_tablist > .dijitTabListWrapper .dijitTab.visible');
+	let returnVal = [],
+			err = '';
+	
+	if( _engine.domTools.test.mainTabType('HCR Cases and Outcomes') )
+		returnVal = $('#HCRCASEAPPWorkspaceSection-stc_tablist > .dijitTabListWrapper .dijitTab.visible');
+	else err = "Could not gather tab list. Not on HCR Cases and Outcomes";
+	
+	if( err ) _engine.debug.warn( `[ get.hcrTabList() ] ${ err }` );
+	
+	return returnVal; 
 	
 });
--- js/modules/domTools/get/hcrTabListTypeQuery.js
+++ js/modules/domTools/get/hcrTabListTypeQuery.js
@@ -1,28 +1,17 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('domTools/get/hcrTabListTypeQuery',function( _queryType ){
-
-	let returnVal = false;
-
+_engine.module.define('domTools/get/hcrTabListTypeQuery',function( reqType ){
+	
+	let returnVal = [],
+			typeMatch = [];
+	
+	if( typeof reqType === 'string' ) typeMatch.push( reqType.toLowerCase() );
+	else if ( Array.isArray( reqType ) ) $.each(reqType,function(k,v){ typeMatch[k] = v.toLowerCase() });
+	
 	$.each( _engine.domTools.get.hcrTabList(), function( k, v ){
 		
-		let tabType = _engine.domTools.test.hcrTabType( $( v ) ).split('|')[0];
+		let tabType = _engine.domTools.test.hcrTabType( $( v ) ).toLowerCase();
 		
-		/*
-		
-		var _tabType = _engine.domTools.test.hcrTabType( $( v ) );
-		
-		if( _tabType.split("|").length == 2 && _queryType.toLowerCase() == "pdc" ){
-
-			_tabType = _tabType.split("|")[0].trim;
-
-		}
-
-		if( _tabType.toLowerCase() == _queryType.toLowerCase() ){
-
-			_returnArray.push( v );
-
-		}
-		*/
+		if( typeMatch.indexOf( tabType ) > -1 ) returnVal.push( v );
 		
 	});
 
