9e19ffabaa867a8bb37ad0d990745b77ba4405af
lpshanley <lpshanley@gmail.com>
2017-01-10 09:53:08 -0500

    Pushing div align plugin to Alpha

--- js/modules/tools/customPlugins/center.js
+++ js/modules/tools/customPlugins/center.js
@@ -1,3 +1,13 @@
 $.fn.center = function(){
-	console.log("Test");
+		
+	let winW = $( window ).width(),
+			objW = $( this ).width(),
+			left = (winW - objW) / 2,
+			winH = $( window ).height(),
+			objH = $( this ).height(),
+			top = (winH - objH) / 2;
+	
+	$( this ).css( 'left', left );
+	$( this ).css( 'top', top );
+	
 }
