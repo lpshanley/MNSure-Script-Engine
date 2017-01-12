efaf2dd2ce2678e16b8207f84044c4d66b2a41b4
lpshanley <lpshanley@gmail.com>
2017-01-12 15:58:47 -0500

    Person searching now allows wildcarding using asterisk

--- js/modules/caseWork/unifiedSearch/_finish.js
+++ js/modules/caseWork/unifiedSearch/_finish.js
@@ -13,14 +13,17 @@ _engine.module.define('caseWork/unifiedSearch/_finish',function( modalId, params
 			if( [8].indexOf( input.length ) > -1 ){
 				//Case Search
 				_engine.search._case(function(){
-					
 					_engine.domTools.set.searches.fieldFill("Reference",input);
-					
+					_engine.tools.selectSearchResult();
 				});
 				
 			}
 			else if( [9,10].indexOf( input.length ) > -1 ){
 				//Person Search
+				_engine.search._person(function(){
+					_engine.domTools.set.searches.fieldFill("Reference",input);
+					_engine.tools.selectSearchResult();
+				});
 				
 			}
 			else {
@@ -28,131 +31,24 @@ _engine.module.define('caseWork/unifiedSearch/_finish',function( modalId, params
 			}
 			
 		}
+		else {
+			
+			let name = input.replace(/\|/g,' ').replace(' ','|').split('|');
+				
+			_engine.search._person(function(){
+				
+				console.log( name );
+				
+				_engine.domTools.set.searches.fieldFill("First Name", name[0]);
+				
+				if( name[1] ) _engine.domTools.set.searches.fieldFill("Last Name", name[1]);
+				
+				_engine.tools.selectSearchResult();
+				
+			});
+			
+		}
 		
 	});
-	
-	/*
 
-	$.each( _engine.storage.modalParams.get(),function(k,v){
-			if( _input.length === 8 ){
-
-				// Case Number
-
-				let _tabToClose = _engine.domTools.get.hcrTabListTypeQuery("Case Search");
-				_engine.tools.closeTabHCR( _tabToClose );
-
-				_engine.search._case();
-
-				_c2 = 0;
-
-				let _openSearch = setInterval(function(){
-					_engine.debug.info("- * Attempting to target search screen [ attempt: "+ _c2 +" ]");
-					if(_c2 <= _engine.advanced._vars.iterations){
-						if( _engine.domTools.test.searches.windowLoaded() ){
-
-							_engine.domTools.set.searches.fieldFill("Reference",_input);
-
-							_engine.tools.selectSearchResult();
-
-							clearInterval( _openSearch );
-						}
-						++_c2;
-					} else {
-						clearInterval( _openSearch );
-					}
-				}, _engine.advanced._vars.timeout);
-				_openSearch;
-
-			} else if ( _input.length === 9 || _input.length === 10 ){
-				// SSN or MNS ID
-
-				let _tabToClose = _engine.domTools.get.hcrTabListTypeQuery("Person Search");
-				_engine.tools.closeTabHCR( _tabToClose );
-
-				_engine.search._person();
-
-				_c2 = 0;
-
-				let _openSearch = setInterval(function(){
-					_engine.debug.info("- * Attempting to target search screen [ attempt: "+ _c2 +" ]");
-					if(_c2 <= _engine.advanced._vars.iterations){
-						if( _engine.domTools.test.searches.windowLoaded() ){
-
-							_engine.domTools.set.searches.fieldFill("Reference",_input);
-
-							_engine.tools.selectSearchResult();
-
-							clearInterval( _openSearch );
-						}
-						++_c2;
-					} else {
-						clearInterval( _openSearch );
-					}
-				}, _engine.advanced._vars.timeout);
-				_openSearch;
-
-			} else {
-
-				//Not a valid input -> Error msg needed here.
-
-			}
-
-		} else {
-			// Name
-
-			_engine.search._person();
-
-			var _name = _input.replace(/\|/g,' ').replace(' ','|').split('|');
-
-			if( _name.length > 1 ){
-
-				_c2 = 0;
-
-				var _openSearch = setInterval(function(){
-					_engine.debug.info("- * Attempting to target search screen [ attempt: "+ _c2 +" ]");
-					if(_c2 <= _engine.advanced._vars.iterations){
-						if( _engine.domTools.test.searches.windowLoaded() ){
-
-							_engine.domTools.set.searches.fieldFill("First Name",_name[0]);
-
-							_engine.domTools.set.searches.fieldFill("Last Name",_name[1]);
-
-							_engine.tools.selectSearchResult();
-
-							clearInterval( _openSearch );
-						}
-						++_c2;
-					} else {
-						clearInterval( _openSearch );
-					}
-				}, _engine.advanced._vars.timeout);
-				_openSearch;
-
-			} else {
-
-				_c2 = 0;
-
-				let _openSearch = setInterval(function(){
-					_engine.debug.info("- * Attempting to target search screen [ attempt: "+ _c2 +" ]");
-					if(_c2 <= _engine.advanced._vars.iterations){
-						if( _engine.domTools.test.searches.windowLoaded() ){
-
-							_engine.domTools.set.searches.fieldFill("First Name",_name[0]);
-
-							clearInterval( _openSearch );
-						}
-						++_c2;
-					} else {
-						clearInterval( _openSearch );
-					}
-				}, _engine.advanced._vars.timeout);
-				_openSearch;
-
-			}
-
-		}
-
-	});
-
-	*/
 });
