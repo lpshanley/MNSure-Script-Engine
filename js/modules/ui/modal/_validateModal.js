72c8daa0fef68ac7356c1b45428825f122d11f00
lpshanley <lpshanley@gmail.com>
2017-01-12 09:33:53 -0500

    Updated modal validation logic

--- js/modules/ui/modal/_validateModal.js
+++ js/modules/ui/modal/_validateModal.js
@@ -1,28 +1,30 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('ui/modal/_validateModal',function( modalId ){
 
-	let _invalidFields = 0,
+	let errors = 0,
 			required = '[data-id='+modalId+'] div.mns-modal-template ',
-			returnVal = false;
+			returnVal = true;
 	
 	_engine.ui.modal._clustersActive( modalId ) ?
 		required += '> .mns-input-group.required, div.mns-modal-template > .input-cluster-active > .required' :
 		required += '.required';
-
+	
+	console.log( required );
+	
 	$.each( $( required ) , function( k,v ){ 
 
 		if( $( v ).find('input').val() === "" ){
 
 			$( v ).addClass("input-error");
 
-			++_invalidFields;
+			++errors;
 
 		} 
 		else $( v ).removeClass("input-error");
 
 	});
 
-	if( _invalidFields === 0 ) returnVal = true;
+	if( errors ) returnVal = false;
 	
 	return returnVal;
 
