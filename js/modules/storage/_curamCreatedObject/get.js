6ba9c1fe11858ee49e21bfd85758dcfc75133a75
lpshanley <lpshanley@gmail.com>
2017-01-18 15:28:22 -0500

    Pushing local to live alpha

--- js/modules/storage/_curamCreatedObject/get.js
+++ js/modules/storage/_curamCreatedObject/get.js
@@ -3,7 +3,7 @@ _engine.module.define('storage/_curamCreatedObject/get',function(){
 	
 	let rtn = false;
 	
-	if( typeof curam.tab.getSelectedTab() === 'object' ) rtn = typeof curam.tab.getSelectedTab();
+	if( typeof curam.tab.getSelectedTab() === 'object' ) rtn = curam.tab.getSelectedTab().params.tabDescriptor;
 	
 	_engine.debug.warn('[storage/_curamCreatedObject/get] function useage is deprecated. Replaced by internal function. Please update function.');
 	
