d60e448dc95510181a1eefa575afd9084e2e9dc9
lpshanley <lpshanley@gmail.com>
2017-01-19 12:24:05 -0500

    Changing persist session to no longer require a full window reload

--- js/modules/events/_persistSession.js
+++ js/modules/events/_persistSession.js
@@ -1,5 +1,15 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('events/_persistSession',function( loading ){
+
+	let tab = _engine.domTools.get.hcrTabActive()
+	
+	_engine.search._case(function(){ 
+		_engine.tools.closeTabHCR( _engine.domTools.get.hcrTabListTypeQuery('Case Search') );
+	});
+	
+	if( tab ) tab.click();
+
+	/*
 	
 	let mainTab, subTab;
 	
@@ -31,5 +41,7 @@ _engine.module.define('events/_persistSession',function( loading ){
 			
 		}
 	}
+	
+	*/
 	
 });
--- js/modules/tools/closeTabHCR.js
+++ js/modules/tools/closeTabHCR.js
@@ -1,7 +1,11 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
 _engine.module.define('tools/closeTabHCR',function( _tab ){
 	
-	typeof _tab == 'undefined' ? _tab = _engine.domTools.get.hcrTabActive() : _tab = _tab[0];
+	typeof _tab == 'undefined' ? 
+		_tab = _engine.domTools.get.hcrTabActive() : 
+		_tab[0] === undefined ?
+			_tab = _tab :
+			_tab = _tab[0];
 
 	$( _tab ).find('span.dijitTabCloseButton').click();
 	
