98d08c73668ee9254954b98bd66758fec0002f0e
lpshanley <lpshanley@gmail.com>
2017-01-19 15:34:33 -0500

    Pushing error modal correction

--- js/modules/caseWork/global/onCaseScreen.js
+++ js/modules/caseWork/global/onCaseScreen.js
@@ -2,11 +2,16 @@
 _engine.module.define('caseWork/global/onCaseScreen',function( callback, showModal ){
 	
 	if( typeof showModal === 'undefined' ) showModal = false;
-		
+
 	_engine.domTools.test.hcrTabActiveIsIC(function( result ){
 		
-		if( result ) if ( typeof callback === 'function' ) callback();
+		if( result ) {
+			if ( typeof callback === 'function' ) 
+				callback();
+		}
 		else {
+			
+			console.log( showModal );
 			
 			if( showModal ){
 			
