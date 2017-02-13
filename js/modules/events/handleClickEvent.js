36f74904041003d2258090f279f012a56706ad6d
lpshanley <lpshanley@gmail.com>
2017-02-13 17:31:43 -0500

    Pushing updated click handler

--- js/modules/events/handleClickEvent.js
+++ js/modules/events/handleClickEvent.js
@@ -1,34 +1,35 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('events/handleClickEvent',['tools/stringToQuery','debug/info'],function( req ){
+_engine.module.define('events/handleClickEvent',['debug/info'],function( req ){
 	
-	req = _engine.tools.stringToQuery( req );
+	req = _engine.tools.splitArg( req );
 	
-	let last = req.length - 1;
+	let last = req.length - 1,
+			module = [req[0] + "/" + req[1]],
+			eventManager = _engine;
 	
-	let eventManager = _engine;
-	
-	$.each(req,function(key,value){
-		
-		let param = '';
-		
-		if( value.indexOf("(") !== -1 ){
-			
-			param = value.substring(value.indexOf("(")+1,value.indexOf(")")); 
-			value = value.substring(0,value.indexOf("(")); 
-			
-		}
-		
-		eventManager = eventManager[value];
-		
-		if( key === last ){
-			
-			if( typeof eventManager === 'function' ) eventManager(param);
-			else{
-				_engine.debug.info( eventManager, param );
+	_engine.module.require(module,function(){
+		$.each(req,function(key,value){
+
+			let param = '';
+
+			if( value.indexOf("(") !== -1 ){
+
+				param = value.substring(value.indexOf("(")+1,value.indexOf(")")); 
+				value = value.substring(0,value.indexOf("(")); 
+
 			}
-			
-		}
-	
+
+			eventManager = eventManager[value];
+
+			if( key === last ){
+
+				if( typeof eventManager === 'function' ) eventManager(param);
+				else{
+					_engine.debug.info( eventManager, param );
+				}
+
+			}
+
+		});
 	});
-	
 });
