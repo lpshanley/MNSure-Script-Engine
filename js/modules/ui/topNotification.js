eb588277cb8c29b59668fe7881c681d20ff46f37
lpshanley <lpshanley@gmail.com>
2017-02-13 16:25:59 -0500

    Adding vars to ticker setup

--- js/modules/ui/topNotification.js
+++ js/modules/ui/topNotification.js
@@ -1,5 +1,5 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('ui/topNotification',{
+_engine.module.define('ui/topNotification',['advanced/_vars'],{
 	
 	add: function( msg ){
 		
