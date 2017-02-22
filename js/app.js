cb1e14228719fee39e4553d574528bd9d71d067c
lpshanley <lpshanley@gmail.com>
2017-02-22 09:21:18 -0500

    Pushing beta

--- js/app.js
+++ js/app.js
@@ -24,75 +24,84 @@ var _engine = {
 			/* Runs the callback after all modules have been requested */
 			_engine.module.loadRequired(function(){
 				
-				_engine.debug.info('All modules have loaded.');
-
-				_engine.tools.loadAddons.run( _engine.tools.loadAddons.config );
-
-				/* Loaded
-				/* Scripts Main Button
-				========================*/
-
-				//********** Right Click **********//
-				// Performs Quick Load of Searches
+				let online = false;
 				
-				$('#script-launcher a').contextmenu(function(e){
+				if( online ) {
 
-						// Prevent context menu pop-up
-					e.preventDefault();
+					_engine.debug.info('All modules have loaded.');
 
-						// Open Case Search
-					_engine.search._case();
+					_engine.tools.loadAddons.run( _engine.tools.loadAddons.config );
 
-						// Open Person Search
-					_engine.search._person();
+					/* Loaded
+					/* Scripts Main Button
+					========================*/
 
-				});
+					//********** Right Click **********//
+					// Performs Quick Load of Searches
 
-				var version = _engine.storage.config.get('commit.current');
+					$('#script-launcher a').contextmenu(function(e){
 
-				version === 'master' ?
-					_engine.storage.debugStatus.set( false ):
-					_engine.storage.debugStatus.set( true );
-				
-				_engine.storage.prefillCache.clear();
-				
-				_engine.ui.topNotification.add(`Script Library: ${version}`);
-				
-				//Dynamic Ticket Notifs (10s)
-				setInterval(function(){
-					
-					_engine.ui.topNotification.remove("Session Expiry");
-					_engine.ui.topNotification.add( `Session Expiry - ${ _engine.advanced._sessionExpiry() }` );
-					
-				},10000);
-				
-				if( version !== 'master' && version !== 'beta' ){
-					
-					$.ajax({
-						url: 'https://api.github.com/rate_limit?access_token=e4ad5080ca84edff38ff06bea3352f30beafaeb1',
-						dataType: 'json',
-						async: false,
-						success: function( data ){
-							_engine.ui.topNotification.add(`Calls Remaining: ${data.resources.core.remaining}`);
-						}
+							// Prevent context menu pop-up
+						e.preventDefault();
+
+							// Open Case Search
+						_engine.search._case();
+
+							// Open Person Search
+						_engine.search._person();
+
+					});
+
+					var version = _engine.storage.config.get('commit.current');
+
+					version === 'master' ?
+						_engine.storage.debugStatus.set( false ):
+						_engine.storage.debugStatus.set( true );
+
+					_engine.storage.prefillCache.clear();
+
+					_engine.ui.topNotification.add(`Script Library: ${version}`);
+
+					//Dynamic Ticket Notifs (10s)
+					setInterval(function(){
+
+						_engine.ui.topNotification.remove("Session Expiry");
+						_engine.ui.topNotification.add( `Session Expiry - ${ _engine.advanced._sessionExpiry() }` );
+
+					},10000);
+
+					if( version !== 'master' && version !== 'beta' ){
+
+						$.ajax({
+							url: 'https://api.github.com/rate_limit?access_token=e4ad5080ca84edff38ff06bea3352f30beafaeb1',
+							dataType: 'json',
+							async: false,
+							success: function( data ){
+								_engine.ui.topNotification.add(`Calls Remaining: ${data.resources.core.remaining}`);
+							}
+						});
+
+					}
+
+					_engine.ui.dom.prepUI(function(){
+
+						_engine.ui.topNotification.run();
+
+							//Build out menu
+						_engine.ui.scriptMenu.refresh();
+
+						_engine.events.domMonitor();
+
+						$('.scripts-link, .center-box').removeAttr('style');
+
+						_engine.advanced.setupTimeoutAlert();
+
 					});
 					
 				}
-				
-				_engine.ui.dom.prepUI(function(){
+				else {
 					
-					_engine.ui.topNotification.run();
-					
-						//Build out menu
-					_engine.ui.scriptMenu.refresh();
-					
-					_engine.events.domMonitor();
-						
-					$('.scripts-link, .center-box').removeAttr('style');
-					
-					_engine.advanced.setupTimeoutAlert();
-					
-				});
+				}
 				
 			});
 			
