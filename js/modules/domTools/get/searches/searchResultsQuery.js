7ad63e4744c9d1dd5dd6c1cbc461c3f9b6c6390d
lpshanley <lpshanley@gmail.com>
2017-02-27 09:50:07 -0500

    Pushing updated reqs to domTOols

--- js/modules/domTools/get/searches/searchResultsQuery.js
+++ js/modules/domTools/get/searches/searchResultsQuery.js
@@ -1,5 +1,5 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('domTools/get/searches/searchResultsQuery',function(){
+_engine.module.define('domTools/get/searches/searchResultsQuery', ['domTools/get/searches/advancedQuery'], function(){
 	
 	return _engine.domTools.get.searches.advancedQuery("table[summary='Search Results. Press INSERT + ESC to update list contents'] tbody tr:not('.list-details-row')");
 	
