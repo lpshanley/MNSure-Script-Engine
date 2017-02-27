590399099a7e06a094f5d3cc09fb8e932dbf73c2
lpshanley <lpshanley@gmail.com>
2017-02-27 09:48:22 -0500

    Testing push of query tools

--- js/modules/domTools/get/searches/inputQuery.js
+++ js/modules/domTools/get/searches/inputQuery.js
@@ -1,5 +1,5 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('domTools/get/searches/inputQuery',function( _title ){
+_engine.module.define('domTools/get/searches/inputQuery', ['domTools/get/searches/advancedQuery'], function( _title ){
 	
 	return _engine.domTools.get.searches.advancedQuery( 'input[title="' + _title + '"]' );
 	
--- js/modules/domTools/set/searches/fieldFill.js
+++ js/modules/domTools/set/searches/fieldFill.js
@@ -1,5 +1,5 @@
 /* MNSure Script Engine | (c) Lucas Shanley | https://raw.githubusercontent.com/lpshanley/MNSure-Script-Engine/master/LICENSE */
-_engine.module.define('domTools/set/searches/fieldFill',function( target, value ){
+_engine.module.define('domTools/set/searches/fieldFill',['domTools/get/searches/inputQuery'], function( target, value ){
 	
 	let field = _engine.domTools.get.searches.inputQuery( target );
 
